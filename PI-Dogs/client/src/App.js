26c8342d36759c70416a60e45469f9f4129b8c85
adolfo4509 <calvo330906@gmail.com>
2021-08-01 19:10:59 -0500

    AVANCES

--- PI-Dogs/client/src/App.js
+++ PI-Dogs/client/src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
-      <button>Home</button>
     </div>
   );
 }
---
+++ PI-Dogs/client/src/Componentes/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <div>
+      <label>Title</label>
+      <input type="text" value="" name="title"></input>
+    </div>
+  );
+}
+export default Home;
