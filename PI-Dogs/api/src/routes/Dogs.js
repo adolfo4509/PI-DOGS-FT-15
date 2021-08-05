a0f1be82e575ddd6c86da13e107f0ac5675b4251
adolfo4509 <calvo330906@gmail.com>
2021-08-05 14:21:35 -0500

    Avances en las rutas

--- PI-Dogs/api/src/routes/Dogs.js
+++ PI-Dogs/api/src/routes/Dogs.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 const { Router } = require("express");
 const { v4: uuidv4 } = require("uuid");
 const { Dogs, Temperament } = require("../db.js");
-const Dog = require("../models/Dog.js");
+const temperament = require("../models/Temperament.js");
 require("dotenv").config();
 const { API_KEY } = process.env;
 require("dotenv").config();
@@ -20,28 +20,39 @@ const getAppInfo = async () => {
   });
   return apiInfo;
 };
-const getDbInfo = async () => {
-  return await Dog.findAll({
-    include: {
-      model: Temperament,
-      attributes: ["name"],
-      trougth: { attribute: [] },
-    },
-  });
-};
-const getAll = async () => {
-  const apiInfo = await getAppInfo();
-  const dbInfo = await getDbInfo();
-  const infoTotal = apiInfo.concat(dbInfo);
-  return infoTotal;
-};
+// const getDbInfo = async () => {
+//   return await temperament.findAll({
+//     include: {
+//       model: Temperament,
+//       attributes: ["name"],
+//       trougth: { attribute: [] },
+//     },
+//   });
+// };
+// const getAll = async () => {
+//   const apiInfo = await getAppInfo();
+//   const dbInfo = await getDbInfo();
+//   const infoTotal = apiInfo.concat(dbInfo);
+//   return infoTotal;
+// };
 
 /*GET /dogs:
 Obtener un listado de las razas de perro
 Debe devolver solo los datos necesarios para la ruta principal
 */
 router.get("/dogs", async (req, res) => {
-  res.send("Estoy desde el Get");
+  const name = req.query.name;
+  const dogsTotal = await getAppInfo();
+  if (name) {
+    let dogName = await dogsTotal.filter((e) =>
+      e.name.toLowerCase().includes(name.toLowerCase())
+    );
+    dogName.length
+      ? res.status(200).send(dogName)
+      : res.status(404).send("No esta el Dog, lo sentimos");
+  } else {
+    res.status(200).send(dogsTotal);
+  }
 });
 
 /*
